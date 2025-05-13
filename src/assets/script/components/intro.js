b3b3e2f6f1c85008aec77214d5aeb0ede7c4ea81
nrak126 <daichidaichi126@gmail.com>
2025-05-13 23:24:01 +0900

    不要なコンポーネントとルーティング機能を削除

--- src/assets/script/components/intro.js
+++
@@ -1,8 +0,0 @@
-// components/intro.js
-export function IntroComponent() {
-  return `
-    <h1 class="title">Minish games</h1>
-		<button class="sign-up"><a href="/src/sign-up.html">アカウント登録</a></button>
-		<button class="how-to">遊び方</button>
-  `;
-}
--- src/assets/script/main.js
+++
@@ -1,5 +0,0 @@
-// main.js
-import { handleRouting } from './router.js';
-
-window.addEventListener("hashchange", handleRouting);
-window.addEventListener("load", handleRouting);
